f959bbc5ab1f17f0ae698dc5b8099ae4855a2d27
Steven Herbst <steven@zeroasic.com>
2022-06-09 15:06:50 -0700

    set the stack pointer

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -2,13 +2,13 @@
 #define EXIT_CODE_ADDR (void*)0x100000
 #define EXIT_CODE      0x3333
 
-static inline void done(int code)
+void done(int code)
 {
 	int* exit = EXIT_CODE_ADDR;
 	*exit = code;
 }
 
-static inline void puts(char* str, volatile int* uart)
+void puts(char* str, volatile int* uart)
 {
 	char* s = str;
 	int c;
@@ -18,7 +18,7 @@ static inline void puts(char* str, volatile int* uart)
 	}
 }
 
-void _start(void)
+void main()
 {
 	int* port = UART_ADDR;
 	puts("Hello World!\n", port);
