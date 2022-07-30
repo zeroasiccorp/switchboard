b9e4509750d1280a8ef9d1706853967ceb50e91c
Steven Herbst <steven@zeroasic.com>
2022-07-29 19:06:46 -0700

    try running tests on github

--- examples/riscv-grid/scripts/hello.sh
+++ examples/riscv-grid/scripts/hello.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# cause exit command to run if we get SIGINT or SIGTERM,
+# which in turn will trigger EXIT.  this is needed to make
+# sure that the simulator background process is killed, since
+# otherwise, it can be quite annoying to clean up.
+
 # ref: https://spin.atomicobject.com/2017/08/24/start-stop-bash-background-process/
+
 trap "exit" INT TERM ERR
-trap "kill 0" EXIT
 
 # figure out where shared memory queues are located
 UNAME_S=$(uname -s)
@@ -16,8 +21,11 @@ fi
 rm -f "${SHMEM_DIR}/queue-5555"
 rm -f "${SHMEM_DIR}/queue-5556"
 
-# launch the verilator simulation as a background process
+# launch the verilator simulation as a background process,
+# and add a trap that will kill the process on exit
 ./verilator/obj_dir/Vtestbench &
+VERILATOR_PID=$!
+trap "kill ${VERILATOR_PID}" EXIT
 
 # launch a client to communicate with the simulation
 ./cpp/client
