1692cba2819b332d697ea121b689c70b83ddd437
Steven Herbst <steven@zeroasic.com>
2022-09-23 17:08:30 -0700

    add router example

---
+++ examples/router/cpp/client.cc
@@ -0,0 +1,40 @@
+#include "switchboard.hpp"
+
+int main() {
+    SBTX tx;
+    SBRX rx;
+    
+    // initialize connections
+
+    tx.init("queue-5555");
+    rx.init("queue-5556");
+
+    // form packet
+
+    sb_packet txp;
+
+    for (int i=0; i<32; i++) {
+        txp.data[i] = i & 0xff;
+    }
+
+    txp.destination = 1;
+    txp.last = 0;
+    
+    // send packet
+
+    tx.send_blocking(txp);
+    printf("TX packet: %s\n", sb_packet_to_str(txp).c_str());
+
+    // receive packet
+
+    sb_packet rxp;
+
+    rx.recv_blocking(rxp);
+    printf("RX packet: %s\n", sb_packet_to_str(rxp).c_str());
+
+    for (int i=0; i<32; i++) {
+        assert(rxp.data[i] == (txp.data[i] + 1));
+    }
+
+    return 0;
+}
