dd7c5a115c1c1f619b2cbe2d869a76199c14231a
Steven Herbst <sgherbst@gmail.com>
2022-10-03 17:46:45 -0700

    fix issue with performance measurement for icarus

--- verilog/sim/perf_meas_sim.sv
+++ verilog/sim/perf_meas_sim.sv
@@ -19,33 +19,32 @@ module perf_meas_sim #(
         endtask
     `else
         import "DPI-C" function void pi_time_taken(output real t);
+        function void init(input integer n);
+            cycles_per_meas = n;
+        endfunction
     `endif
 
-    function void init(input integer n);
-        cycles_per_meas = n;
-    endfunction
+    initial begin
+        /* verilator lint_off IGNOREDRETURN */
+        pi_time_taken(t);  // discard first result since it is invalid
+        /* verilator lint_on IGNOREDRETURN */
+    end
 
-	initial begin
-		/* verilator lint_off IGNOREDRETURN */
-		pi_time_taken(t);  // discard first result since it is invalid
-		/* verilator lint_on IGNOREDRETURN */
-	end
-
-	always @(posedge clk) begin
-		if (total_clock_cycles >= cycles_per_meas) begin
-			/* verilator lint_off IGNOREDRETURN */
-			pi_time_taken(t);
-			/* verilator lint_on IGNOREDRETURN */
-			sim_rate = (1.0*total_clock_cycles)/t;
-			if (sim_rate < 1.0e6) begin
-				$display("Simulation rate: %0.3f kHz", 1e-3*sim_rate);
-			end else begin
-				$display("Simulation rate: %0.3f MHz", 1e-6*sim_rate);
-			end
-			total_clock_cycles <= 0;
-		end else begin
-			total_clock_cycles <= total_clock_cycles + 1;
-		end
-	end
+    always @(posedge clk) begin
+        if (total_clock_cycles >= cycles_per_meas) begin
+            /* verilator lint_off IGNOREDRETURN */
+            pi_time_taken(t);
+            /* verilator lint_on IGNOREDRETURN */
+            sim_rate = (1.0*total_clock_cycles)/t;
+            if (sim_rate < 1.0e6) begin
+                $display("Simulation rate: %0.3f kHz", 1e-3*sim_rate);
+            end else begin
+                $display("Simulation rate: %0.3f MHz", 1e-6*sim_rate);
+            end
+            total_clock_cycles <= 0;
+        end else begin
+            total_clock_cycles <= total_clock_cycles + 1;
+        end
+    end
 
 endmodule
