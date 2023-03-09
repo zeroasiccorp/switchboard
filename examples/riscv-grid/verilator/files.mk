f41700f503172ea9e149d9a3393fc1e510045f11
Noah Moroze <noah@zeroasic.com>
2023-03-09 17:02:52 -0500

    Move verilog-axi from examples/deps -> deps/
    sb_fpga_queues depends on this submodule, and this is a part of
    Switchboard itself, not just one of its examples.

--- examples/riscv-grid/verilator/files.mk
+++ examples/riscv-grid/verilator/files.mk
@@ -10,10 +10,10 @@ obj_dir/Vtestbench: timescale.sv \
 	../verilog/axi_to_umi.v \
 	../verilog/umi_to_axi.v \
 	../verilog/picorv32.v \
-	../../deps/verilog-axi/rtl/arbiter.v \
-	../../deps/verilog-axi/rtl/priority_encoder.v \
-	../../deps/verilog-axi/rtl/axil_interconnect.v \
-	../../deps/verilog-axi/rtl/axil_dp_ram.v \
+	../../../deps/verilog-axi/rtl/arbiter.v \
+	../../../deps/verilog-axi/rtl/priority_encoder.v \
+	../../../deps/verilog-axi/rtl/axil_interconnect.v \
+	../../../deps/verilog-axi/rtl/axil_dp_ram.v \
 	../verilog/axil_interconnect_wrap_1x2.v \
 	../verilog/dut.v \
 	../verilog/testbench.sv \
