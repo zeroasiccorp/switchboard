5354b1194bf681ff783fcfe716dd5e7682b22859
Steven Herbst <sgherbst@gmail.com>
2023-06-20 17:05:42 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 # the pybind module is built as _switchboard that is imported
 # into the module called "switchboard".  this will allow us
