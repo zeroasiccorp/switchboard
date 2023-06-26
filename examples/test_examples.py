f0a56592eb07a536eb7da6b00e500b3da7d41ab6
Steven Herbst <sgherbst@gmail.com>
2023-06-26 15:52:02 -0700

    pytest working locally

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,54 +1,38 @@
 #!/usr/bin/env python3
 
 import pytest
+from pathlib import Path
 
 # import "test_cmd" utility function without the "test_" prefix
 # this prevents pytest from incorrectly identifying this function
 # as a test function to be run directly
 from switchboard.test_util import test_cmd as tcmd
 
-
-@pytest.mark.parametrize('mode', ['verilator', 'icarus'])
-def test_minimal(mode):
-    tcmd(['make', mode], 'PASS!', path='minimal')
+THIS_DIR = Path(__file__).resolve().parent
 
 
-def test_old_ebrick_cpu_verif():
-    tcmd('make', 'Hello World!', path='old_ebrick_cpu_verif')
-
-
-def test_old_riscv_grid():
-    tcmd(['make', 'hello'], 'Hello World!', path='old_riscv_grid')
-
-
-def test_old_umidriver():
-    tcmd('make', ['correct: 2', 'incorrect: 0'], path='old_umidriver')
-
-
-@pytest.mark.parametrize('mode', ['python', 'cpp'])
-def test_old_umiram(mode):
-    tcmd(['make', mode], 'PASS!', path='old_umiram')
-
-
-@pytest.mark.parametrize('mode', ['python', 'cpp'])
-def test_umiram(mode):
-    tcmd(['make', mode], path='umiram')
-
-
-@pytest.mark.parametrize('path', [
-    'python',
-    'router',
-    'stream',
-    'tcp'
+@pytest.mark.parametrize('path,expected,target', [
+    ['minimal', 'PASS!', 'verilator'],
+    ['minimal', 'PASS!', 'icarus'],
+    ['old_ebrick_cpu_verif', 'Hello World!', None],
+    ['old_riscv_grid', 'Hello World!', 'hello'],
+    ['old_umidriver', ['correct: 2', 'incorrect: 0'], None],
+    ['old_umiram', 'PASS!', 'python'],
+    ['old_umiram', 'PASS!', 'cpp'],
+    ['umiram', None, 'python'],
+    ['umiram', None, 'cpp'],
+    ['python', 'PASS!', None],
+    ['router', 'PASS!', None],
+    ['stream', 'PASS!', None],
+    ['tcp', 'PASS!', None],
+    ['umi_endpoint', None, None],
+    ['umi_fifo', None, None],
+    ['umi_splitter', None, None]
 ])
-def test_for_pass(path):
-    tcmd('make', ['PASS!'], path=path)
+def test_make(path, expected, target):
+    cmd = ['make']
 
+    if target is not None:
+        cmd += [target]
 
-@pytest.mark.parametrize('path', [
-    'umi_endpoint',
-    'umi_fifo',
-    'umi_splitter'
-])
-def test_basic(path):
-    tcmd('make', path=path)
+    tcmd(cmd, expected, path=THIS_DIR / path)
