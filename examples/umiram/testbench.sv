69f35c0f866672829d01b60487992bffab6623e6
Noah Moroze <noah@zeroasic.com>
2023-07-09 00:32:17 -0400

    Specify FST for Verilator dump

--- examples/umiram/testbench.sv
+++ examples/umiram/testbench.sv
@@ -66,7 +66,7 @@ module testbench (
 
     initial begin
         if ($test$plusargs("trace")) begin
-            $dumpfile("testbench.vcd");
+            $dumpfile("testbench.fst");
             $dumpvars(0, testbench);
         end
     end
