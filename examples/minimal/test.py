c16d43eea3e26f32e1861ce4d01dd76ff430deb0
Noah Moroze <noah@zeroasic.com>
2023-07-21 19:10:21 -0400

    Support Icarus testbench builds in SbDut (#86)
    * Support Icarus testbench builds in SbDut
    
    Compiling VPI modules and running the DUT still happen outside of SC.
    
    * Bump SC

--- examples/minimal/test.py
+++ examples/minimal/test.py
@@ -4,15 +4,30 @@ import time
 from argparse import ArgumentParser
 from pathlib import Path
 
-from switchboard import delete_queue, binary_run, verilator_run, icarus_run
+from switchboard import SbDut, delete_queue, binary_run, verilator_run, icarus_run
 
 THIS_DIR = Path(__file__).resolve().parent
+
+
+def build_testbench():
+    dut = SbDut('testbench', tool='icarus')
+
+    # Set up inputs
+    dut.input('testbench.sv')
+
+    # Build simulator
+    dut.run()
+
+    return dut.find_result('vvp', step='compile')
 
 
 def main(mode="verilator"):
     # clean up old queues if present
     for q in ["client2rtl.q", "rtl2client.q"]:
         delete_queue(q)
+
+    if mode == 'icarus':
+        icarus_bin = build_testbench()
 
     # start client and chip
     # this order yields a smaller waveform file
@@ -21,7 +36,7 @@ def main(mode="verilator"):
     if mode == 'verilator':
         verilator_run(THIS_DIR / 'obj_dir' / 'Vtestbench', plusargs=['trace'])
     elif mode == 'icarus':
-        icarus_run(THIS_DIR / 'testbench.vvp', modules=[THIS_DIR / 'switchboard_vpi.vpi'],
+        icarus_run(icarus_bin, modules=[THIS_DIR / 'switchboard_vpi.vpi'],
             extra_args=['-fst'])
     else:
         raise ValueError(f'Unknown mode: {mode}')
