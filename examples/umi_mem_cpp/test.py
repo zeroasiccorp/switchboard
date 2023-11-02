6df27f2cec63c25a5b17c98557f82c9a20e61955
Edgar E. Iglesias <edgar@zeroasic.com>
2023-11-02 08:39:07 +0100

    Add a umi_mem_cpp example based on griddle-bigmem
    Signed-off-by: Edgar E. Iglesias <edgar@zeroasic.com>

---
+++ examples/umi_mem_cpp/test.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Zero ASIC
+"""
+Basic reusable test for a memory chiplet
+"""
+
+import numpy as np
+from util import (get_dtype_from_umi_size, dtype_random_data, umi_atomic_op)
+from switchboard import (UmiTxRx, umi_pack, PyUmiPacket, UmiCmd, delete_queue,
+        binary_run)
+
+
+def memory_check(umi, device, test_rdma=False):
+    print("### MINIMAL EXAMPLE ###")
+
+    umi.write(0x1234, np.uint32(0xDEADBEEF))
+    rdval = umi.read(0x1234, np.uint32)
+    print(f"Read: {rdval}")
+    assert rdval == 0xDEADBEEF
+
+    umi.write(0x2345, np.arange(65, dtype=np.uint8))
+    rdval = umi.read(0x2345, 65)
+    print(f"Read: {rdval}")
+    assert (rdval == np.arange(65, dtype=np.uint8)).all()
+
+    umi.write(0x0, np.uint8(12))
+    val1 = umi.atomic(0x0, np.uint8(23), 'add')
+    val2 = umi.read(0x0, np.uint8)
+    print(f"Atom: {val1}, Read: {val2}")
+    assert val1 == 12
+    assert val2 == 12 + 23
+
+    if test_rdma:
+        # write some data to a known location
+        umi.write(0x3450, np.uint8([0xa, 0xb, 0xc, 0xd]))
+
+        # send an RDMA request
+        rdma_req = PyUmiPacket(
+            cmd=umi_pack(UmiCmd.UMI_REQ_RDMA, 0, 0, 3, 1, 1, 0, 0, 0),
+            dstaddr=0x3450,
+            srcaddr=0x4560
+        )
+        umi.send(rdma_req)
+        print('Sent RDMA request.')
+
+        # check the RDMA response (which will come back as a posted write)
+        p = device.recv()
+        print(f"Received: {p.data}")
+        assert np.array_equal(p.data, [0xa, 0xb, 0xc, 0xd])
+
+    print("### WRITES ###")
+
+    # 1 byte
+    wrbuf = np.array([0xBAADF00D], np.uint32).view(np.uint8)
+    for i in range(4):
+        umi.write(0x10 + i, wrbuf[i])
+
+    # 2 bytes
+    wrbuf = np.array([0xB0BACAFE], np.uint32).view(np.uint16)
+    for i in range(2):
+        umi.write(0x20 + 2 * i, wrbuf[i])
+
+    # 4 bytes
+    umi.write(0x30, np.uint32(0xDEADBEEF))
+
+    # 8 bytes
+    umi.write(0x40, np.uint64(0xBAADD00DCAFEFACE))
+
+    # 64 bytes
+    wrbuf = np.arange(64, dtype=np.uint8)
+    umi.write(0x50, wrbuf)
+
+    print("### READS ###")
+
+    # 1 byte
+    rdbuf = np.empty((4,), dtype=np.uint8)
+    for i in range(4):
+        rdbuf[i] = umi.read(0x10 + i, np.uint8)
+    val32 = rdbuf.view(np.uint32)[0]
+    print(f"Read: 0x{val32:08x}")
+    assert val32 == 0xBAADF00D
+
+    # 2 bytes
+    rdbuf = np.empty((2,), dtype=np.uint16)
+    for i in range(2):
+        rdbuf[i] = umi.read(0x20 + 2 * i, np.uint16)
+    val32 = rdbuf.view(np.uint32)[0]
+    print(f"Read: 0x{val32:08x}")
+    assert val32 == 0xB0BACAFE
+
+    # 4 bytes
+    val32 = umi.read(0x30, np.uint32)
+    print(f"Read: 0x{val32:08x}")
+    assert val32 == 0xDEADBEEF
+
+    # 8 bytes
+    val64 = umi.read(0x40, np.uint64)
+    print(f"Read: 0x{val64:016x}")
+    assert val64 == 0xBAADD00DCAFEFACE
+
+    # 64 bytes
+    rdbuf = umi.read(0x50, 64)
+    print("Read: {" + ", ".join([f"0x{elem:02x}" for elem in rdbuf]) + "}")
+    assert (rdbuf == np.arange(64, dtype=np.uint8)).all()
+
+    print("### ATOMICS ###")
+
+    for size in [0, 1, 2, 3]:
+        for cmd in ['swap', 'add', 'and', 'or', 'xor', 'min', 'max', 'minu', 'maxu']:
+            # get the datatype
+            dtype = get_dtype_from_umi_size(size=size)
+
+            # get operands
+            x = dtype_random_data(dtype)
+            y = dtype_random_data(dtype)
+
+            # test operation
+            run_atomic(umi=umi, prev=x, data=y, cmd=cmd, sram_base=0)
+
+
+def run_atomic(umi, prev, data, cmd, sram_base):
+    print(f"* Atomic {cmd.upper()}, dtype={prev.dtype}")
+
+    # determine the expected result of the atomic operation
+    try:
+        expected = umi_atomic_op(prev=prev, data=data, cmd=cmd)
+    except ValueError:
+        assert False, f"Invalid atomic operation {cmd}"
+
+    # set memory to a specific value
+    umi.write(sram_base, prev)
+
+    # read old value
+    val1 = umi.read(sram_base, prev.dtype)
+    print(f"Read: 0x{val1:x}")
+    assert val1 == prev
+
+    # apply atomic operation
+    val2 = umi.atomic(sram_base, data, cmd)
+    print(f"Atom: 0x{val2:x}")
+    assert val2 == prev
+
+    # read new value
+    val3 = umi.read(sram_base, prev.dtype)
+    print(f"Read: 0x{val3:x}")
+    assert val3 == expected
+
+
+if __name__ == '__main__':
+    # clean up old queues if present
+    from_client = 'mem-req-rx.q'
+    to_client = 'mem-rep-tx.q'
+    queues = [from_client, to_client]
+
+    for q in queues:
+        delete_queue(q)
+
+    umi = UmiTxRx(from_client, to_client)
+    binary_run(bin='./umi_mem', args=None)
+    memory_check(umi=umi, device=None, test_rdma=False)
---
+++ examples/umi_mem_cpp/util.py
@@ -0,0 +1,108 @@
+# General utilities for running tests.
+
+# Copyright (C) 2023 Zero ASIC
+
+import numpy as np
+import contextlib
+
+
+@contextlib.contextmanager
+def numpy_overflow_warning_off():
+    """Context manager that temporarily disables the numpy overflow warning."""
+
+    over = np.geterr()['over']
+    np.seterr(over='ignore')
+
+    yield
+
+    np.seterr(over=over)
+
+
+def get_dtype_from_umi_size(size, signed=False):
+    """
+    Returns a numpy datatype for a given UMI size and signedness.
+    For example, get_dtype_from_umi_size(0) returns np.uint8, while
+    get_dtype_from_umi_size(1, True) returns np.int16.
+    """
+
+    # make sure that the provided size is valid.
+    assert size in {0, 1, 2, 3}, 'Only UMI sizes between 0 and 3 (inclusive) are allowed.'
+
+    # determine the name of the datatype
+    name = f"{'u' if not signed else 'i'}{1<<size}"
+
+    # return the dtype
+    return np.dtype(name)
+
+
+def dtype_random_data(dtype, min_val=None, max_val=None):
+    """Returns random data for the given data type between its minimum
+    and maximum bounds.  The argument can be np.[u]int{8,16,32,64}"""
+
+    if min_val is None:
+        min_val = np.iinfo(dtype).min
+
+    if max_val is None:
+        max_val = np.iinfo(dtype).max
+
+    return np.random.randint(min_val, max_val + 1, dtype=dtype)
+
+
+def numpy_signed_view(value):
+    """
+    Returns a view of the given value as a signed integer.
+    For example, numpy_signed_view(np.uint8(255)) should
+    yield "-1" with a dtype of int8.
+    """
+
+    return value.view(np.dtype(f'i{value.itemsize}'))
+
+
+def numpy_unsigned_view(value):
+    """
+    Returns a view of the given value as an unsigned integer.
+    For example, numpy_unsigned_view(np.int8(-1)) should
+    yield "255" with a dtype of uint8.
+    """
+
+    return value.view(np.dtype(f'u{value.itemsize}'))
+
+
+def umi_atomic_op(prev, data, cmd):
+    """
+    Returns the expected result of a UMI atomic operation, i.e. the
+    value expected to be read from memory immediately following an
+    atomic operation.  "prev" is the previous value in memory, and
+    "data" is the operand provided in the UMI packet.  "cmd" should
+    be a member of the UmiCmd enum.
+    """
+
+    # make sure the data types are the same
+    assert prev.dtype is data.dtype, ("Data types must be the same for both"
+        " operands of an atomic operation.")
+
+    if (cmd.upper() == 'SWAP'):
+        return data
+    elif (cmd.upper() == 'ADD'):
+        # "add" may overflow, but that is not illegal per the UMI specification.
+        # for example, the 8-bit operation 0xff + 0xff should yield 0xfe, not 0x1fe.
+        # however, numpy will complain if overflow occurs, which adds noise to
+        # the output of the test, so we need to temporarily disable the numpy warning.
+        with numpy_overflow_warning_off():
+            return (prev + data)
+    elif (cmd.upper() == 'AND'):
+        return (prev & data)
+    elif (cmd.upper() == 'OR'):
+        return (prev | data)
+    elif (cmd.upper() == 'XOR'):
+        return (prev ^ data)
+    elif (cmd.upper() == 'MIN'):
+        return min(numpy_signed_view(prev), numpy_signed_view(data)).view(prev.dtype)
+    elif (cmd.upper() == 'MAX'):
+        return max(numpy_signed_view(prev), numpy_signed_view(data)).view(prev.dtype)
+    elif (cmd.upper() == 'MINU'):
+        return min(numpy_unsigned_view(prev), numpy_unsigned_view(data)).view(prev.dtype)
+    elif (cmd.upper() == 'MAXU'):
+        return max(numpy_unsigned_view(prev), numpy_unsigned_view(data)).view(prev.dtype)
+    else:
+        raise ValueError("Invalid atomic operation.")
