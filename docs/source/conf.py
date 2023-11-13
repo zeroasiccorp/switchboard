b039a4c1e543af2b80c95b6047f7b9bcf66773b7
bsheffer1 <ben@zeroasic.com>
2023-11-13 15:46:14 -0700

    WIP configuring autodocumentation based on doc strings

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../switchboard/'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Switchboard'
+copyright = '2023, Zero ASIC'
+author = 'Zero ASIC'
+release = '0.0.29'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # automatically generate documentation for modules
+    "sphinx.ext.napoleon",  # to read Google-style or Numpy-style docstrings
+    "sphinx.ext.viewcode",  # to allow vieing the source code in the web page
+    "autodocsumm"  # to generate tables of functions, attributes, methods, etc.
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+root_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# don't include docstrings from the parent class
+autodoc_inherit_docstrings = False
+# Show types only in descriptions, not in signatures
+autodoc_typehints = "description"
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Switchboard documentation master file, created by
+   sphinx-quickstart on Thu Nov  9 14:01:11 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Switchboard's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
