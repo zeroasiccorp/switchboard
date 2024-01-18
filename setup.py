244a318526e69ed6a6e9615d78790f6986168045
Steven Herbst <sgherbst@gmail.com>
2024-01-18 11:35:15 -0800

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-__version__ = "0.0.32"
+__version__ = "0.0.33"
 
 #################################################################################
 # parse_reqs, long_desc from https://github.com/siliconcompiler/siliconcompiler #
