85090f12a7283705e51aa0cfebf30fdb1e91e76a
Steven Herbst <sgherbst@gmail.com>
2024-05-08 12:14:51 -0700

    make tests self-checking, add TCP=1 option

--- tests/test.py
+++ tests/test.py
@@ -3,97 +3,92 @@
 # Copyright (c) 2024 Zero ASIC Corporation
 # This code is licensed under Apache License 2.0 (see LICENSE for details)
 
-import os
+import time
 import atexit
-import subprocess
 import argparse
 import itertools
 
 from pathlib import Path
+from switchboard import binary_run, delete_queue, start_tcp_bridge
 
 THIS_DIR = Path(__file__).resolve().parent
 TOP_DIR = THIS_DIR.parent
-SHMEM_DIR = THIS_DIR
+
+QUEUE_COUNTER = itertools.count(start=0)
+
+
+def next_queue():
+    queue = str(THIS_DIR / f'queue-{next(QUEUE_COUNTER)}')
+    delete_queue(queue)
+    atexit.register(lambda queue=queue: delete_queue(queue))
+    return queue
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', action='store_true')
-    parser.add_argument('--test', type=str, default='hello')
+    parser.add_argument('--test', type=str, default='hello',
+        choices=['hello', 'bandwidth', 'latency'])
     parser.add_argument('--iterations', type=int, default=None)
+    parser.add_argument('--tcp', action='store_true')
     args = parser.parse_args()
 
     # set defaults
     if args.iterations is None:
         if args.test == 'bandwidth':
-            args.iterations = 50000000
+            args.iterations = 50000000 if not args.tcp else 40000
         elif args.test == 'latency':
-            args.iterations = 5000000
+            args.iterations = 5000000 if not args.tcp else 5000
 
-    # provide unique queue numbers
-    queue_counter = itertools.count(start=5555)
-
-    # rx queue numbers
-    rx = [None, None]
-
+    # determine the number of paths
     if args.test in {'hello', 'bandwidth'}:
-        rx[1] = next(queue_counter)
+        num_paths = 1
     elif args.test in {'latency'}:
-        rx[0] = next(queue_counter)
-        rx[1] = next(queue_counter)
+        num_paths = 2
     else:
         raise Exception(f'Unknown test: {args.test}')
 
-    # split ports
-    tx = [None, None]
-    tx[0] = rx[1]
-    tx[1] = rx[0]
+    # create the paths
+    paths = []
+    port = 5555
+    for _ in range(num_paths):
+        a = next_queue()
 
-    # clean up old queues if present
-    for port in set(rx + tx):
-        if port is not None:
-            filename = str(SHMEM_DIR / f'queue-{port}')
-            try:
-                os.remove(filename)
-            except OSError:
-                pass
+        if args.tcp:
+            b = next_queue()
+
+            print(f'Starting TCP bridge at port {port}... ', end='', flush=True)
+
+            start_tcp_bridge(inputs=[a], port=port)
+            start_tcp_bridge(outputs=[('*', b)], port=port)
+
+            time.sleep(2)
+            print('done')
+
+            port += 1
+        else:
+            b = a
+
+        paths.append((a, b))
 
     # run the specific test of interest
     if args.test == 'hello':
         hello = THIS_DIR / 'hello.out'
-        p = run_cmd(hello, 'rx', rx[1], auto_exit=False, verbose=args.verbose)
-        run_cmd(hello, 'tx', tx[0], verbose=args.verbose)
-        p.wait()
+        p = binary_run(hello, ['rx', paths[0][1]])
+        binary_run(hello, ['tx', paths[0][0]])
+        exit(p.wait())
     elif args.test == 'bandwidth':
         bandwidth = THIS_DIR / 'bandwidth.out'
-        p = run_cmd(bandwidth, 'rx', rx[1], args.iterations, auto_exit=False, verbose=args.verbose)
-        run_cmd(bandwidth, 'tx', tx[0], args.iterations, verbose=args.verbose)
-        p.wait()
+        p = binary_run(bandwidth, ['rx', paths[0][1], args.iterations])
+        binary_run(bandwidth, ['tx', paths[0][0], args.iterations])
+        exit(p.wait())
     elif args.test == 'latency':
         latency = THIS_DIR / 'latency.out'
-        run_cmd(latency, 'second', rx[0], tx[0], args.iterations, verbose=args.verbose)
-        p = run_cmd(latency, 'first', rx[1], tx[1], args.iterations,
-            auto_exit=False, verbose=args.verbose)
-        p.wait()
+        binary_run(latency, ['second', paths[0][1], paths[1][0], args.iterations])
+        p = binary_run(latency, ['first', paths[1][1], paths[0][0], args.iterations])
+        exit(p.wait())
     else:
         raise Exception(f'Unknown test: {args.test}')
-
-
-def run_cmd(path, *args, auto_exit=True, verbose=False):
-    cmd = []
-    cmd += [path]
-    cmd += args
-    cmd = [str(elem) for elem in cmd]
-
-    if verbose:
-        print(' '.join(cmd))
-
-    p = subprocess.Popen(cmd)
-
-    if auto_exit:
-        atexit.register(p.terminate)
-
-    return p
 
 
 if __name__ == '__main__':
